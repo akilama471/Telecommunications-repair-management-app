ea84f0915f47a84ea4ba2b8b27eb502ce493cf61
akilama471 <akilamadusanka471@gmail.com>
2022-11-09 22:38:24 +0530

    improved assets

---
+++ lib/config/assests_store.dart
@@ -0,0 +1,16 @@
+class AssetStore {
+  //Apps Logos
+  static const String brandLogo = "assets/images/logos/brand.png";
+  static const String appLogo = "assets/images/logos/app.png";
+
+  //Apps Headers
+  static const String taskHeader = "assets/images/headers/task.png";
+  static const String permissionHeader = "assets/images/headers/permission.png";
+
+  //Apps Icons
+  static const String chatIcon = "assets/images/icons/chat.png";
+  static const String dashboardIcon = "assets/images/icons/dashboard.png";
+  static const String profileIcon = "assets/images/icons/profile.png";
+  static const String searchIcon = "assets/images/icons/search.png";
+  static const String settingIcon = "assets/images/icons/settings.png";
+}
